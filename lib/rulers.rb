2c5ae79ac5e42c3264c75345f73eaf82487d7224
Ryan Yogan <ryanyogan@gmail.com>
2013-02-04 01:00:27 -0600

    Added basic routing to controllers, only controller/action omittion params

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -1,11 +1,25 @@
 require "rulers/version"
+require "rulers/routing"
 require "rulers/array"
+
 
 module Rulers
   class Application
     def call(env)
+      klass, act = get_controller_and_action(env)
+      controller = klass.new(env)
+      text = controller.send(act)
       [200, {'Content-Type' => 'text/html'},
-       ["Hello from Ruby on Rulers!"]]
+       [text]]
     end
+  end
+
+  class Controller
+    attr_reader :env
+
+    def initialize(env)
+      @env = env
+    end
+
   end
 end
