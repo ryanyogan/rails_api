e19a9402bd4f85d52dbe217d69137bf8388f96af
Ryan Yogan <ryanyogan@gmail.com>
2013-02-04 14:10:40 -0600

    Controllers respond to real responses now, params are easier to grav, main app uses controller get_response

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -16,16 +16,15 @@ module Rulers
                 {'Content-Type' => 'text/html'}, []]
       end
 
-      klass, act = get_controller_and_action(env)
-      controller = klass.new(env)
-      begin
-        text = controller.send(act)
-        [200, {'Content-Type' => 'text/html'},
-         [text]]
-      rescue RuntimeError => e
-        [500, {'Content-Type' => 'text/html'},
-         ["Oh crap, something broke! Look at the stack trace.\n #{e}"]]
-      end
+      klass, action = get_controller_and_action env
+      controller    = klass.new env
+      text          = controller.send action
+      if controller.get_response
+        st, hd, rs = controller.get_response.to_a
+        [st, hd, [rs.body].flatten]
+      else
+        [200, {'Content-Type' => 'text/html'}, [text]]
+      end 
     end
   end
 end
--- lib/rulers/controller.rb
+++ lib/rulers/controller.rb
@@ -11,6 +11,33 @@ module Rulers
       @env = env
     end
 
+    # Let's cache the result here if it doesn't
+    # exist
+    def request
+      @request ||= Rack::Request.new @env
+    end
+
+    def response(text, status = 200, headers = {})
+      raise "Already responded!" if @response
+      a = [text].flatten
+      @response = Rack::Response.new(a, status, headers)
+    end
+
+    def get_response # Internal use only
+      @response # attr_reader would make more sense
+    end
+
+    def render_response(*args)
+      response(render(*args))
+    end
+
+    # Thanks to Rack, we now have a parsed out 
+    # environment hash, hence we can extrapolate 
+    # "rails like" params very easily
+    def params
+      request.params
+    end
+
     def controller_name
       klass = self.class
       klass = klass.to_s.gsub /Controller$/, ""
