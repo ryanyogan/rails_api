16efde6b592f69d7211a0e8cddb509c53ef27359
Ryan Yogan <ryanyogan@gmail.com>
2013-02-05 10:37:42 -0600

    More bug fixes

--- lib/nails.rb
+++ lib/nails.rb
@@ -11,14 +11,14 @@ require "nails/array" # This doesn't do anything other than show "Monkey Patchin
 module Nails
   class Application
     def call(env)
-      if env["PATH_INFO"] == '/favicon.ico'
+      if env['PATH_INFO'] == '/favicon.ico'
         return [404,
-                {'Content-Type' => 'text/html'}, []]
+          {'Content-Type' => 'text/html'}, []]
       end
 
-      klass, action = get_controller_and_action env
-      rack_app = klass.action action
-      rack_app.call env
+      klass, act = get_controller_and_action(env)
+      rack_app = klass.action(act)
+      rack_app.call(env)
     end
   end
 end
