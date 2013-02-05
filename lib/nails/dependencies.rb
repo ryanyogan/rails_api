c27eaffe76dd8770434454a42d4b9e136c2c9cb7
Ryan Yogan <ryanyogan@gmail.com>
2013-02-05 10:39:43 -0600

    Using rack we no longer need the additional work around

--- lib/nails/dependencies.rb
+++ lib/nails/dependencies.rb
@@ -1,13 +1,8 @@
 # lib/nails/dependencies.rb
 
 class Object
-  def self.const_missing c
-    return nil if @calling_const_missing
-    @calling_const_missing = true
+  def self.const_missing(c)
     require Nails.to_underscore(c.to_s)
-    klass = Object.const_get c
-    @calling_const_missing = false
-
-    klass
+    Object.const_get(c)
   end
 end
